de40705d9da35cd7445ac04652d248d3bec230a6
axitbhalgamiya <axitnb9999@gmail.com>
2022-01-05 11:23:54 +0530

    first commit

---
+++ fifthjan/app/src/main/java/com/example/fifthjan/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.fifthjan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView t1,t2;
+SeekBar s;
+RatingBar r;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1=(TextView) findViewById(R.id.t1);
+        t2=(TextView) findViewById(R.id.t2);
+        s= (SeekBar) findViewById(R.id.s);
+        r=(RatingBar) findViewById(R.id.rb);
+        s.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                t1.setText("Rating:"+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        r.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                t2.setText("You rating :"+v);
+            }
+        });
+    }
+
+}
